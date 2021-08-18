188a3093692fc7c9562751e5ac5a2fa6bacdf682
Diego <diego.aviz.junges@accenture.com>
2021-08-17 23:07:58 -0300

    feat: finished account component

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  coverageProvider: 'babel'
+  coverageProvider: 'babel',
+  watchPathIgnorePatterns: ['globalConfig']
 }
---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,13 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accounCollection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await accounCollection.insertOne(accountData)
+    const account = await accounCollection.findOne(insertedId)
+    return MongoHelper.map(account)
+  }
+}
