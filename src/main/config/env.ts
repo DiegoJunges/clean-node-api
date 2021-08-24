08fcfa825736d4d9b3b9bce52206aa78d11ea133
Diego <diego.aviz.junges@accenture.com>
2021-08-23 21:54:54 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://admin:eaeOWJNyKJ1OAHGj@cluster0.ogurm.mongodb.net/smartranking?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}
