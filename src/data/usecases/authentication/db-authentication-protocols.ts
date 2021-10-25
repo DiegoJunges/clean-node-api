828471097624016bb0d01143263709654eab3a45
Diego <diego.aviz.junges@accenture.com>
2021-10-24 21:34:06 -0300

    feat: authentication

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,6 @@
+export * from '../../../domain/models/account'
+export * from '../../../domain/usecases/authentication'
+export * from '../../protocols/criptography/hash-comparer'
+export * from '../../protocols/criptography/encrypter'
+export * from '../../protocols/update-access-token-repository'
+export * from '../../protocols/load-account-by-email-repository'
